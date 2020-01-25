f611d0dc30a4ecda0ecbd2b3d38ce9a6a7a89fe7
Alcides Junior <alcidesjunior.infor@gmail.com>
2020-01-24 22:10:00 -0300

    initials

---
+++ MVVMStudy/MainViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  MVVMStudy
+//
+//  Created by Alcides Junior on 24/01/20.
+//  Copyright © 2020 Alcides Junior. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+    }
+
+
+}
+
---
+++ MVVMStudy/SceneDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  SceneDelegate.swift
+//  MVVMStudy
+//
+//  Created by Alcides Junior on 24/01/20.
+//  Copyright © 2020 Alcides Junior. All rights reserved.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
+        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
+        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        window?.windowScene = windowScene
+        let rootScene = MainViewController()
+        window?.rootViewController = rootScene
+        window?.makeKeyAndVisible()
+    }
+
+}
+
